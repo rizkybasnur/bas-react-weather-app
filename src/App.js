ef24cbabf2841a24181ed60c5b045b4cf4d93638
bas <rizkyrahmansyahbasnur@gmail.com>
2022-05-16 10:49:15 +0700

    delete comment

--- src/App.js
+++ src/App.js
@@ -154,6 +154,3 @@ function App({ isScriptLoaded, isScriptLoadSuccess }) {
   );
 }
 export default App;
-// export default scriptLoader([
-//   `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`,
-// ])(App);
